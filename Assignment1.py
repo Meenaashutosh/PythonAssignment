dddd86ddb89e66095d4d905aa383ad1dff06ad91
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-05 17:58:43 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,8 @@
+#1. Write a python script for creating directory,displaying its contents.
+
+import os,sys
+os.mkdir("Ashutosh")
+os.chdir("Ashutosh")
+os.system("touch file1")
+os.system ("ls")
+os.system("cat file1")
