f917dffc372cc029f510fc3be46b7e63efeec597
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 15:36:48 +0530

    Update Assignment15.py

--- Assignment15.py
+++ Assignment15.py
@@ -1,5 +1,11 @@
-"""15. Write a program to shuffle and print the list [3,6,7,8]."""
+'''15. Write a program to shuffle and print the list [3,6,7,8].'''
+
 import random
 list = [3,6,7,8]
 random.shuffle(list)
 print list
+
+#output
+'''
+[6, 7, 3, 8]
+'''
