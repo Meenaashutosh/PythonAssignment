e1e9a59abd308c48ac4138953d85561ce626639b
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 14:27:05 +0530

    Update Assignment9.py

--- Assignment9.py
+++ Assignment9.py
@@ -1,3 +1,5 @@
+
+
 #9.Define a function max_of_three() that takes three numbers as arguments and returns the largest of them.
 def max_of_three(a,b,c):
     #DocString
@@ -12,8 +14,17 @@ def max_of_three(a,b,c):
             return b
         else:
             return c
-print ("Function to find the largest of three numbers")
-firstNum=int(input("Enter the first  number"))
-secondNum=int(input("Enter the second  number"))
-thirdNum =int(input("Enter the third  number"))
-print ("Largest of " +str(firstNum) +", " +str(secondNum) +", " +str(thirdNum) +" is " +str(max_of_three(firstNum,secondNum,thirdNum)))
+print ("Function finding the largest number among three numbers")
+firstNum=int(input("Enter the first  number:"))
+secondNum=int(input("Enter the second  number:"))
+thirdNum =int(input("Enter the third  number:"))
+print ("Largest number from  " +str(firstNum) +", " +str(secondNum) +", " +str(thirdNum) +" is " +str(max_of_three(firstNum,secondNum,thirdNum)))
+
+#Output
+'''
+Function finding the largest number among three numbers
+Enter the first  number: 45
+Enter the second  number: 23
+Enter the third  number: 26
+Largest number from 45, 23, 26 is 45
+'''
