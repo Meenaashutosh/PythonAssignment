dc9b5bb27da09ee83154174acf0c9caf8438ff38
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 15:40:26 +0530

    Update Assignment16.py

--- Assignment16.py
+++ Assignment16.py
@@ -1,4 +1,10 @@
 #16.Write a program to print the list after removing delete even numbers in [5,6,77,45,22,12,24].
+
 list = [5,6,77,45,22,12,24]
 list = [x for x in list if x%2!=0]
 print list
+
+#output
+'''
+[5, 77, 45]
+'''
