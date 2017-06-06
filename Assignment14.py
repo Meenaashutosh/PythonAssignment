490c2e58c1c6ed124191018dcc708367416fbf52
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 15:59:01 +0530

    Update Assignment14.py

--- Assignment14.py
+++ Assignment14.py
@@ -1,4 +1,15 @@
 #14.Write a program to read an ASCII string and to convert it to a unicode string encoded by utf-8.
+
 s = raw_input("Enter the ASCII Value")
 u = unicode( s ,"utf-8")
 print u
+
+#output--
+'''
+Enter the ASCII Value f
+f
+not getting proper output
+'''
+
+
+
