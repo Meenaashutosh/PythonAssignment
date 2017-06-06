28cd996be2fee4ca380d82575bec58bba09700de
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 14:21:50 +0530

    Update Assignment9.py

--- Assignment9.py
+++ Assignment9.py
@@ -1,9 +1,8 @@
-#9. Define a function max_of_three() that takes three numbers as arguments and returns the largest of them.
-
+#9.Define a function max_of_three() that takes three numbers as arguments and returns the largest of them.
 def max_of_three(a,b,c):
     #DocString
-    """ finding out the maximum of three numbers """
-  if a > b:
+    """ Function to find out the maximum of three numbers """
+    if a > b:
         if a > c:
             return a
         else:
@@ -13,3 +12,8 @@ def max_of_three(a,b,c):
             return b
         else:
             return c
+print ("Function to find the largest of three numbers")
+firstNum=int(input("Enter the first  number"))
+secondNum=int(input("Enter the second  number"))
+thirdNum =int(input("Enter the third  number"))
+print ("Largest of " +str(firstNum) +", " +str(secondNum) +", " +str(thirdNum) +" is " +str(max_of_three(firstNum,secondNum,thirdNum)))
