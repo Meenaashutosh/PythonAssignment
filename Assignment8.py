8dd8968b064a0d4b29e83e062016320baf39059b
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 09:56:42 +0530

    Update Assignment8.py

--- Assignment8.py
+++ Assignment8.py
@@ -1,3 +1,4 @@
+
 #8. Define a function max() that takes two numbers as arguments and returns the largest of them.
 
 def max(a, b):
@@ -13,3 +14,10 @@ if __name__ == "__main__":
     print max(2, 4)
     print max(4, 4)
     print max(4, 2)
+
+    #output
+    '''
+    python Assignment8.py 
+    4 
+    None
+    4'''
