62525c87235caf50acd6ad04b160621cd6792a10
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 14:40:42 +0530

    Add files via upload

---
+++ Assignment13.py
@@ -0,0 +1,8 @@
+"""13.With a given tuple (1,2,3,4,5,6,7,8,9,10), write a program to print the first half 
+values in one line and the last half values in one line."""
+
+tuple=(1,2,3,4,5,6,7,8,9,10)
+tuple1=tuple[:5]
+tuple2=tuple[5:]
+print tuple1
+print tuple2
