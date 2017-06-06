644d4eefd5145fd9ba3f4e5f36af34482e04598d
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 15:51:52 +0530

    Update Assignment20.py

--- Assignment20.py
+++ Assignment20.py
@@ -1,4 +1,5 @@
 #20.Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+
 def palindrome_no(s):
    if len(s)<1:
 	return true
@@ -12,3 +13,11 @@ if(palindrome(a)==true):
 	print("string is palindrome..")
 else:
 	print("string is not a palindrome")
+#output -error
+'''
+Enter string abba
+Traceback (most recent call last):  
+File "Assignment20.py",
+line 10, in <module>    a=str(input("Enter string"))  File "<string>", line 1, in <module>
+NameError: name 'abba' is not defined
+'''
