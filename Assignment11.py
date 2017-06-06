14a80a0ff93e0f180b6450a9b8c98fb601462da7
Meenaashutosh <ashutoshmeena111@gmail.com>
2017-06-06 16:37:10 +0530

    Update Assignment11.py

--- Assignment11.py
+++ Assignment11.py
@@ -1,8 +1,8 @@
-""11.Write a program that accepts a sentence and calculate the number of letters and digits.
+'''11.Write a program that accepts a sentence and calculate the number of letters and digits.
               Suppose the following input is supplied to the program:
               i/p: Hello Priya 1287
               o/p: LETTERS 10
-                     DIGITS 4"""
+                     DIGITS 4'''
 s = raw_input("Enter the Sentence")
 d={"DIGITS":0, "LETTERS":0}
 for c in s:
